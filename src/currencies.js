8112107493d0e62f323f9faee87d903859ae0219
Wojciech Rybacki <duttybwooy@gmail.com>
2023-01-25 20:29:09 +0100

    added currencies board

---
+++ src/currencies.js
@@ -0,0 +1,18 @@
+export const currencies = [
+    {
+        short: "EUR",
+        rate: 4.67,
+    },
+    {
+        short: "GBP",
+        rate: 5.34,
+    },
+    {
+        short: "USD",
+        rate: 4.40,
+    },
+    {
+        short: "CHF",
+        rate: 4.75,
+    },
+];
